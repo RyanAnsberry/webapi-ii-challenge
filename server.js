7b4e43d41a0821d1fff47a090ac688310b797643
RyanAnsberry <48343707+RyanAnsberry@users.noreply.github.com>
2019-08-15 18:57:52 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 
-const helmet = require('helmet');
-
 const cors = require('cors');
 
 const PostsRouter = require('./posts-router.js');
@@ -10,7 +8,6 @@ const server = express();
 
 server.use(express.json());
 server.use(cors());
-server.use(helmet());
 server.use('/api/posts', PostsRouter);
 
 server.get('/', (req, res) => {
